4c12dc9743fb9966b49f62da3597eef8c86a682f
Glitch (hello-webpage) <none>
2023-04-30 13:43:13 +0000

    🏤🌥 Checkpoint ./script.js:55545027/30 ./sp-code.js:55545027/5818

--- sp-code.js
+++ sp-code.js
@@ -1,5 +1,18 @@
 export function spCode() {
   return `
+  
+        let purpleSphere = shape(() => {
+  color(1, 0, 1);
+  displace(.4, .4, 0);
+  sphere(0.2);
+});
+
+purpleSphere();
+sphere(0.2);
+      
       box(vec3(.5));
+      
+      
+
   `;
 }
